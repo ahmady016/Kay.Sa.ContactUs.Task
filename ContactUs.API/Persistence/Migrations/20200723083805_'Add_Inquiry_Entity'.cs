4de04b0dc526e5a5a6795cecf93cd238bff03095
ahmady016 <ahmady09@gmail.com>
2020-07-23 10:49:04 +0200

    Add [Migration, Update Database] Add index.html

---
+++ ContactUs.API/Persistence/Migrations/20200723083805_'Add_Inquiry_Entity'.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class Add_Inquiry_Entity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inquiries",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(150)", nullable: true),
+                    Notes = table.Column<string>(type: "nvarchar(1000)", nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    ModifiedAt = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<string>(type: "varchar(100)", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "varchar(100)", nullable: true),
+                    Email = table.Column<string>(type: "varchar(150)", nullable: true),
+                    Phone = table.Column<string>(type: "char(11)", nullable: true),
+                    Message = table.Column<string>(type: "nvarchar(1000)", nullable: true)
+                },
+                constraints: table => {
+                    table.PrimaryKey("PK_Inquiries", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inquiries_Email",
+                table: "Inquiries",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL"
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "Inquiries");
+        }
+    }
+}
